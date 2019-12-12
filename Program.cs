69fda60eb84bed2b794b97fa304c542331f45053
Francisco <peter@peterdeveloper.com>
2019-12-13 00:35:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curso_de_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World FerreiraDEv!");
+
+            Console.ReadLine();
+        }
+    }
+}
